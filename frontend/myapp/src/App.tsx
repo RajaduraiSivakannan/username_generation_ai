735c3e6002ef6200da88613ada48c7223a845c5e
RajaduraiSivakannan <rdurai9976@gmail.com>
2024-10-05 01:31:16 +0530

    hi

---
+++ frontend/myapp/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Nicknamepage } from './components/frontend_page';
+
+function App() {
+  return (
+    <>
+    <Nicknamepage></Nicknamepage></>
+  );
+}
+
+export default App;
