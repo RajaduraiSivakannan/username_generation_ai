e7ff3ad0f8fd08fc21a6f1fd5ad475aa007b8c3d
RajaduraiSivakannan <rdurai9976@gmail.com>
2024-10-05 01:05:59 +0530

    front end added

---
+++ frontend/myapp/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Nicknamepage } from './components/frontpage';
+
+function App() {
+  return (
+    <Nicknamepage></Nicknamepage>
+  );
+}
+
+export default App;
